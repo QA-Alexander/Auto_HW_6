3bd74663f95104d6401991ba2cd88190eb22ba00
Alexander Ryzhakov <aryzhakov87@gmail.com>
2025-09-09 22:08:40 +1000

    Test passed

---
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -0,0 +1,52 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.data.DataHelper;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class DashboardPage {
+    private final String balanceStart = "баланс: ";
+    private final String balanceFinish = " р.";
+    private final SelenideElement heading = $("[data-test-id='dashboard']");
+    private final ElementsCollection cards = $$(".list__item div");
+    private final SelenideElement reloadButton = $("[data-test-id='action-reload']");
+
+    public DashboardPage () {
+        heading.shouldBe(Condition.visible);
+    }
+
+    public int getCardBalance (DataHelper.CardInfo cardInfo) {
+        var text = getCard(cardInfo).getText();
+        return extractBalance(text);
+    }
+
+    public TransferPage selectCardToTransfer(DataHelper.CardInfo cardInfo) {
+        getCard(cardInfo).$("button").click();
+        return new TransferPage();
+
+    }
+
+    private SelenideElement getCard(DataHelper.CardInfo cardInfo) {
+        return cards.findBy(Condition.attribute("data-test-id", cardInfo.getTestID()));
+    }
+
+    public void reloadDashboardPage() {
+        reloadButton.click();
+        heading.shouldBe(Condition.visible);
+    }
+
+    private int extractBalance(String text) {
+        var start = text.indexOf(balanceStart);
+        var finish = text.indexOf(balanceFinish);
+        var value = text.substring(start + balanceStart.length(), finish);
+        return Integer.parseInt(value);
+    }
+
+    public void checkCardBalance (DataHelper.CardInfo cardInfo, int expectedBalance) {
+        getCard(cardInfo).shouldBe(Condition.visible).should(Condition.text(balanceStart + expectedBalance + balanceFinish));
+    }
+}
---
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -0,0 +1,50 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.page.DashboardPage;
+import ru.netology.page.LoginPage;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static ru.netology.data.DataHelper.generateInvalidAmount;
+import static ru.netology.data.DataHelper.generateValidAmount;
+
+public class MoneyTransferTest {
+    DashboardPage dashboardPage;
+    DataHelper.CardInfo firstCardInfo;
+    DataHelper.CardInfo secondCardInfo;
+    int firstCardBalance;
+    int secondCardBalance;
+
+    @BeforeEach
+    void setup() {
+        var loginPage = Selenide.open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationCodePage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCode();
+        dashboardPage = verificationCodePage.validVerify(verificationCode);
+        firstCardInfo = DataHelper.getFirstCardInfo();
+        secondCardInfo = DataHelper.getSecondCardInfo();
+        firstCardBalance = dashboardPage.getCardBalance(firstCardInfo);
+        secondCardBalance = dashboardPage.getCardBalance(secondCardInfo);
+    }
+
+    @Test
+    void shouldTransferFromFirstToSecond() {
+        var amount = generateValidAmount(firstCardBalance);
+        var expectedBalanceFirstCard = firstCardBalance - amount;
+        var expectedBalanceSecondCard = secondCardBalance + amount;
+        var transferPage = dashboardPage.selectCardToTransfer(secondCardInfo);
+        dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), firstCardInfo);
+        dashboardPage.reloadDashboardPage();
+        assertAll(
+                () -> dashboardPage.checkCardBalance(firstCardInfo, expectedBalanceFirstCard),
+                () -> dashboardPage.checkCardBalance(secondCardInfo, expectedBalanceSecondCard)
+        );
+    }
+
+
+
+}
